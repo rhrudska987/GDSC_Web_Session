81f7380b61ac65ad5fc6415e56954642b8c63745
rhrudska987 <rhrudska987@naver.com>
2023-09-21 21:00:50 +0900

    2주차

---
+++ src/main/java/com/mysite/sbb/QuestionService.java
@@ -0,0 +1,31 @@
+package com.mysite.sbb;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()) {
+            return question.get();
+        }
+        else{
+            throw new DataNotFoundException("question not found");
+        }
+    }
+
+}
