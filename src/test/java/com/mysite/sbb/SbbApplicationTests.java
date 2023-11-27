a6c384ac3735c77d1f2cd4d032cf853e2790a489
고경남 <rhrudska987@naver.com>
2023-11-27 17:58:37 +0900

    완성

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -20,116 +20,116 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class SbbApplicationTests {
 
-    @Autowired
-    private QuestionRepository questionRepository;
-
-    @Autowired
-    private AnswerRepository answerRepository;
-
-    @Autowired
-    private QuestionService questionService;
-
+//    @Autowired
+//    private QuestionRepository questionRepository;
+//
+//    @Autowired
+//    private AnswerRepository answerRepository;
+//
+//    @Autowired
+//    private QuestionService questionService;
+//
+////    @Test
+////    void testJpa() {
+////        for (int i = 1; i <= 300; i++) {
+////            String subject = String.format("테 스 트 데 이 터 입 니 다:[%03d]", i);
+////            String content = "내 용 무";
+////            this.questionService.create(subject, content);
+////        }
+////    }
+//
 //    @Test
-//    void testJpa() {
-//        for (int i = 1; i <= 300; i++) {
-//            String subject = String.format("테 스 트 데 이 터 입 니 다:[%03d]", i);
-//            String content = "내 용 무";
-//            this.questionService.create(subject, content);
+//    void testJpa2() {
+//        List<Question> all = questionRepository.findAll();
+//        assertEquals(2, all.size());
+//
+//        Question q = all.get(0);
+//        assertEquals("sbb가 무엇인가요?", q.getSubject());
+//    }
+//
+//    @Test
+//    void testJpa3() {
+//        Optional<Question> oq = this.questionRepository.findById(1);
+//        if(oq.isPresent()) {
+//            Question q = oq.get();
+//            assertEquals("sbb가 무엇인가요?", q.getSubject());
 //        }
 //    }
-
-    @Test
-    void testJpa2() {
-        List<Question> all = questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
-    }
-
-    @Test
-    void testJpa3() {
-        Optional<Question> oq = this.questionRepository.findById(1);
-        if(oq.isPresent()) {
-            Question q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
-        }
-    }
-
-    @Test
-    void testJpa4() {
-        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
-        assertEquals(1, q.getId());
-    }
-
-    @Test
-    void testJpa5() {
-        Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-        assertEquals(1, q.getId());
-    }
-
-    @Test
-    void testJpa6() {
-        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-        Question q = qList.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
-    }
-
-    @Test
-    void testJpa7() {
-        Optional<Question> oq = this.questionRepository.findById(1);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-        q.setSubject("수정된 제목");
-        this.questionRepository.save(q);
-    }
-
-    @Test
-    void testJpa8() {
-        assertEquals(2, this.questionRepository.count());
-        Optional<Question> oq = this.questionRepository.findById(1);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-        this.questionRepository.delete(q);
-        assertEquals(1, this.questionRepository.count());
-    }
-
-    @Test
-    void testJpa9() {
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q);
-        a.setCreateDate(LocalDateTime.now());
-        this.answerRepository.save(a);
-    }
-
-    @Test
-    void testJpa10() {
-        Optional<Answer> oa = this.answerRepository.findById(1);
-        assertTrue(oa.isPresent());
-        Answer a = oa.get();
-        assertEquals(2, a.getQuestion().getId());
-    }
-
-    @Transactional
-    @Test
-    void testJpa11() {
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        List<Answer> answerList = q.getAnswerList();
-
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
-    }
-
-    @Test
-    void contextLoads() {
-    }
+//
+//    @Test
+//    void testJpa4() {
+//        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+//        assertEquals(1, q.getId());
+//    }
+//
+//    @Test
+//    void testJpa5() {
+//        Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+//        assertEquals(1, q.getId());
+//    }
+//
+//    @Test
+//    void testJpa6() {
+//        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+//        Question q = qList.get(0);
+//        assertEquals("sbb가 무엇인가요?", q.getSubject());
+//    }
+//
+//    @Test
+//    void testJpa7() {
+//        Optional<Question> oq = this.questionRepository.findById(1);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//        q.setSubject("수정된 제목");
+//        this.questionRepository.save(q);
+//    }
+//
+//    @Test
+//    void testJpa8() {
+//        assertEquals(2, this.questionRepository.count());
+//        Optional<Question> oq = this.questionRepository.findById(1);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//        this.questionRepository.delete(q);
+//        assertEquals(1, this.questionRepository.count());
+//    }
+//
+//    @Test
+//    void testJpa9() {
+//        Optional<Question> oq = this.questionRepository.findById(2);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//
+//        Answer a = new Answer();
+//        a.setContent("네 자동으로 생성됩니다.");
+//        a.setQuestion(q);
+//        a.setCreateDate(LocalDateTime.now());
+//        this.answerRepository.save(a);
+//    }
+//
+//    @Test
+//    void testJpa10() {
+//        Optional<Answer> oa = this.answerRepository.findById(1);
+//        assertTrue(oa.isPresent());
+//        Answer a = oa.get();
+//        assertEquals(2, a.getQuestion().getId());
+//    }
+//
+//    @Transactional
+//    @Test
+//    void testJpa11() {
+//        Optional<Question> oq = this.questionRepository.findById(2);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//
+//        List<Answer> answerList = q.getAnswerList();
+//
+//        assertEquals(1, answerList.size());
+//        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+//    }
+//
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
